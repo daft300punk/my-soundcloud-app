6a509171332554d2e1393ca81fdf7832ea858a6b
Abhilash Singh <daft300punk@gmail.com>
2017-03-21 19:16:13 +0530

    Style updates

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -4,10 +4,10 @@ import './topbar.css';
 const Topbar = () => {
   return (
     <div className="topbar-wrap">
-      <div className="logo-wrap"><img className="logo" src={require("./logo.png")}></img></div>
+      <div className="logo-wrap"><img className="logo" src={require("./logo.png")} alt="logo"></img></div>
       <div className="input-wrap">
         <input hint="Search songs, artists, audio"></input>
-        <img src={require('./search.png')}></img>
+        <img src={require('./search.png')} alt="search"></img>
       </div>
     </div>
   );
