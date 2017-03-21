3563eb9fe5b7ef46570b35145279a60846275b09
Abhilash Singh <daft300punk@gmail.com>
2017-03-21 18:16:48 +0530

    Added searchbar

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -5,6 +5,10 @@ const Topbar = () => {
   return (
     <div className="topbar-wrap">
       <div className="logo-wrap"><img className="logo" src={require("./logo.png")}></img></div>
+      <div className="input-wrap">
+        <input hint="Search songs, artists, audio"></input>
+        <img src={require('./search.png')}></img>
+      </div>
     </div>
   );
 }
