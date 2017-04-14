7c204cff97fb09abe5ba2bdef9ea946aeaf7f68a
Abhilash Singh <daft300punk@gmail.com>
2017-04-14 14:47:34 +0530

    server.js file

--- server/server.js
+++ server/server.js
@@ -3,3 +3,61 @@ import Express from 'express';
 import qs from 'qs';
 
 import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config';
+
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+
+import { configureStore } from '../src/index';
+import App from '../src/containers/App';
+
+const app = new Express();
+const port = 3000;
+
+const compiler = webpack(webpackConfig);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }))
+app.use(webpackHotMiddleware(compiler));
+
+app.use(handleRender);
+
+function handleRender(req, res) {
+  const store = configureStore();
+
+  const html = renderToString(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  const finalState = store.getState();
+  res.send(renderFullPage(html, finalState));
+}
+
+function renderFullPage(html, preloadedState) {
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>Redux Universal Example</title>
+      </head>
+      <body>
+        <div id="root">${html}</div>
+        <script>
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\x3c')}
+        </script>
+        <script src="/static/bundle.js"></script>
+      </body>
+    </html>
+    `;
+}
+
+app.listen(port, (error) => {
+  if(error) {
+    console.log(error);
+  } else {
+    console.info(`==> 🌎  Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`);
+  }
+});
