ea7daafad3685817b8d1b8efa942d441cb34e89e
Abhilash Singh <daft300punk@gmail.com>
2017-05-07 17:58:46 +0530

    Refactor

--- client/index.js
+++ client/index.js
@@ -4,8 +4,21 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '../src/index';
 import App from '../src/containers/App';
+import rootReducer from '../src/reducers';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 
-const store = configureStore();
+const preloadedState = window.__PRELOADED_STATE__;
+delete window.__PRELOADED_STATE__;
+
+const middleware = [thunk, createLogger()];
+
+const store = createStore(rootReducer,
+  preloadedState,
+  applyMiddleware(...middleware)
+);
+
 const rootElement = document.getElementById('root');
 
 render(
