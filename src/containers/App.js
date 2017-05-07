957337ca6f052580960bb8d1d5f38d13c3542157
Abhilash Singh <daft300punk@gmail.com>
2017-05-07 17:58:46 +0530

    Refactor

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import initializeSC from '../api/initializeSC';
 import TrackListContainer from './TrackListContainer';
 import LeftSidebarContainer from './LeftSidebarContainer';
 import TopbarContainer from './TopbarContainer';
 import RightSidebarContainer from './RightSidebarContainer';
 import BottomBarContainer from './BottomBarContainer';
+import fetchTop50Tracks from '../actions/getTop50Action';
+import { connect } from 'react-redux';
+
 import './App.scss';
 
 initializeSC();
 
-const App = function () {
-  return (
-    <div className="app">
-      <TopbarContainer />
-      <div className="main-wrap">
-        <LeftSidebarContainer />
-        <TrackListContainer />
-        <RightSidebarContainer />
-      </div>
-      <BottomBarContainer />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default App;
+  componentDidMount() {
+    this.props.fetchTop50();
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <TopbarContainer />
+        <div className="main-wrap">
+          <LeftSidebarContainer />
+          <TrackListContainer />
+          <RightSidebarContainer />
+        </div>
+        <BottomBarContainer />
+      </div>
+    );
+  }
+
+}
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchTop50: () => { dispatch(fetchTop50Tracks()); }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
