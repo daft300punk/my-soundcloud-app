797d0b3c287a49585f65a566712acf2258dced49
Abhilash Singh <daft300punk@gmail.com>
2017-05-08 19:15:02 +0530

    Refactor: airbnb eslint spec

--- src/components/Track/index.js
+++ src/components/Track/index.js
@@ -2,53 +2,57 @@ import React, { PropTypes } from 'react';
 import './track.scss';
 import { playStates } from '../../constants/PlayState';
 
+const playIcon = require('./play_icon.png');
+const pauseIcon = require('./pause_icon.png');
+
 const Track = ({
-  artwork_url,
+  artworkUrl,
   title,
   id,
   startPlay,
   playState,
   playingTrackId,
   pause }) => {
-  
   let button;
 
   const styleImg = {
     width: '64px',
     height: '64px',
-  }
-  //Only show pause on the track that is currently playing
+  };
+  // Only show pause on the track that is currently playing
   if (playState === playStates.PLAYING && playingTrackId === id) {
-    button = <div className="play-track show" onClick={() => { pause() }}>
-               <img style={styleImg} src={require('./pause_icon.png')} alt="pause" />
-             </div>
+    button = (
+      <div className="play-track show" role="button" onClick={() => { pause(); }}>
+        <img style={styleImg} src={pauseIcon} alt="pause" />
+      </div>
+    );
   } else {
-    button = <div className="play-track"  onClick={() => { startPlay(id) }}>
-               <img style={styleImg} src={require('./play_icon.png')} alt="pause" />
-             </div>;
+    button = (
+      <div className="play-track" role="button" onClick={() => { startPlay(id); }}>
+        <img style={styleImg} src={playIcon} alt="pause" />
+      </div>
+    );
   }
 
   return (
     <div className="track">
       <div className="track-artwork-wrapper">
-        <img src={artwork_url} alt={title}></img>
-        <div className="artwork-overlay"></div>
+        <img src={artworkUrl} alt={title} />
+        <div className="artwork-overlay" />
         {button}
       </div>
-      {/*<div className="track-detail">
-        {title}
-      </div>*/}
     </div>
-  )
+  );
 };
 
 Track.propTypes = {
-  artwork_url: PropTypes.string,
+  artworkUrl: PropTypes.string,
   title: PropTypes.string,
   id: PropTypes.number,
   startPlay: PropTypes.func.isRequired,
   playingTrackId: PropTypes.number,
   pause: PropTypes.func.isRequired,
+  playState: PropTypes.string,
 };
 
 export default Track;
