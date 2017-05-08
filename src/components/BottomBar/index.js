25fff90c8631a9ce8a4ce567438fcd960aa5acc6
Abhilash Singh <daft300punk@gmail.com>
2017-05-09 01:08:58 +0530

    Bottombar dummy elements

--- src/components/BottomBar/index.js
+++ src/components/BottomBar/index.js
@@ -4,6 +4,13 @@ import './bottomBar.scss';
 const BottomBar = () => {
   return (
     <div className="bottom-bar">
+      <div className="img-wrap">
+      </div>
+
+      <div className="info">
+        <h5>DARK HORSE</h5>
+        <p>Prism - Katy Perry</p>
+      </div>
     </div>
   );
 }
