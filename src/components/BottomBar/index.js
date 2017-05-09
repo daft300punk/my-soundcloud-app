2a26fa2f2c34037c5e334b2df8a5e8ab5cea7e42
Abhilash Singh <daft300punk@gmail.com>
2017-05-09 06:10:00 +0530

    Bottom bar dummy complete

--- src/components/BottomBar/index.js
+++ src/components/BottomBar/index.js
@@ -11,6 +11,24 @@ const BottomBar = () => {
         <h5>DARK HORSE</h5>
         <p>Prism - Katy Perry</p>
       </div>
+      <div className="controls">
+        <img src={require('./Previous.png')} role="button"/>
+        <img src={require('./Pause.png')} role="button"/>
+        <img src={require('./Next.png')} role="button"/>
+      </div>
+      <div className="seek-slider">
+        <span>3:55</span>
+        <input type="range" name="seek" id="points" min="0" max="100" />
+        <span>5:32</span>
+      </div>
+      <div className="repeat-shuffle">
+        <img src={require('./Repeat.png')} role="button"/>
+        <img src={require('./Shuffle.png')} role="button"/>
+      </div>
+      <div className="volume-slider">
+        <img src={require('./Audio.png')} />
+        <input type="range" name="volume" id="volume" min="0" max="100" />
+      </div>
     </div>
   );
 }
