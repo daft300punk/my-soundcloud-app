6091c9b844f2975e8f4db3d0f3e9fd6fcf25cfa4
Abhilash Singh <daft300punk@gmail.com>
2017-05-17 04:18:01 +0530

    Updated all possible flow types for action

--- src/actions/flowType.js
+++ src/actions/flowType.js
@@ -6,6 +6,10 @@ export type Action =
 | { type: 'RECEIVE_TRACKLIST', trackList: Array<any>} // TODO: change type of tracklist
 | { type: 'REQUEST_GET_PLAYER', streamUrl: ?string, id: ?number }
 | { type: 'RECEIVE_PLAYER', player: Object, pos: number, pos: number, currentTimeInSec: number, endTimeInSec: number }
+| { type: 'TRACK_START_PLAYING', pos: number, currentTimeInSec: number }
+| { type: 'TRACK_FINISHED_PLAYING' }
+| { type: 'TRACK_PAUSE' }
+| { type: 'UPDATE_CURRENT_TIME', currentTimeInSec: number }
 ;
 
 export type Dispatch = 
