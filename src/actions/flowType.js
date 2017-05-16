d5565c9612567e67c580f3e1d73eaad4f98afa1f
Abhilash Singh <daft300punk@gmail.com>
2017-05-17 04:11:51 +0530

    Flow types for getstate, dispatch, action, and thunk

--- src/actions/flowType.js
+++ src/actions/flowType.js
@@ -1,9 +1,18 @@
 //@flow
 import * as actionTypes from '../constants/ActionTypes';
 
-export type actionFType =
+export type Action =
   { type: 'REQUEST_TRACKLIST' }
 | { type: 'RECEIVE_TRACKLIST', trackList: Array<any>} // TODO: change type of tracklist
 | { type: 'REQUEST_GET_PLAYER', streamUrl: ?string, id: ?number }
 | { type: 'RECEIVE_PLAYER', player: Object, pos: number, pos: number, currentTimeInSec: number, endTimeInSec: number }
 ;
+
+export type Dispatch = 
+  (action: Action | ThunkAction | PromiseAction | Array<Action>) => any;
+
+export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
+
+export type PromiseAction = Promise<Action>;
+
+export type GetState = () => Object;
