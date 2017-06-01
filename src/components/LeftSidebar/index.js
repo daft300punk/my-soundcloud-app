f42c93adc311086d747757e07c7731d69cd7c9b4
Abhilash Singh <daft300punk@gmail.com>
2017-06-01 19:30:13 +0530

    Update LeftSidebar component to handle category state

--- src/components/LeftSidebar/index.js
+++ src/components/LeftSidebar/index.js
@@ -1,46 +1,61 @@
 import React from 'react';
 import './leftSidebar.scss';
+import { MAIN_CATEGORIES as MCT } from '../../constants/Categories';
+import PropTypes from 'prop-types';
 
-/*
-Sidebar Options:
-A. Top Trending
-  1. Music
-  2. Audio
-B. New and Hot
-  1. Music
-  2. Audio
-C. Explore Genre
-  1. Music
-  2. Audio
-*/
-
-const LeftSidebar = () => {
+const LeftSidebar = ({
+  changeCategory,
+  trackType,
+  kind
+}) => {
+  const isActive = (k, t) => {
+    if(t == trackType && k == kind)
+      return 'active';
+  };
   return (
     <div className="leftsidebar-wrap" id="leftsidebar">
-      {/*<select className="musicOrAudio">
-        <option>Music</option>
-        <option>Audio</option>
-      </select>*/}
       <div className="main-cat">Music</div>
-      <div className="category active">
-        <img src={require('./img/top.png')}/>
+      <div className={'category ' + isActive('top', 'music')}
+        onClick={() => {
+          changeCategory(MCT.music.kind.top, MCT.music.genre['all-music'], 'music');
+        }}
+      >
+        <img src={require('./img/top.png')} />
         <span>Top 50</span>
       </div>
-      <div className="category">
-        <img src={require('./img/hot.png')}/>
+      <div className={'category ' + isActive('trending', 'music')}
+        onClick={() => {
+          changeCategory(MCT.music.kind.trending, MCT.music.genre['all-music'], 'music');
+        }}
+      >
+        <img src={require('./img/hot.png')} />
         <span>Hot & New</span>
       </div>
       <div className="main-cat">Audio</div>
-      <div className="category">
-        <img src={require('./img/top.png')}/>
+      <div className={'category ' + isActive('top', 'audio')}
+        onClick={() => {
+          changeCategory(MCT.audio.kind.top, MCT.audio.genre['all-audio'], 'audio');
+        }}
+      >
+        <img src={require('./img/top.png')} />
         <span>Top 50</span>
       </div>
-      <div className="category">
-        <img src={require('./img/hot.png')}/>
+      <div className={'category ' + isActive('trending', 'audio')}
+        onClick={() => {
+          changeCategory(MCT.audio.kind.trending, MCT.audio.genre['all-audio'], 'audio');
+        }}
+      >
+        <img src={require('./img/hot.png')} />
         <span>Hot & New</span>
       </div>
     </div>
   );
+};
+
+LeftSidebar.propTypes = {
+  changeCategory: PropTypes.func.isRequired,
+  trackType: PropTypes.string.isRequired,
+  kind: PropTypes.string.isRequired
 };
 
 export default LeftSidebar;
