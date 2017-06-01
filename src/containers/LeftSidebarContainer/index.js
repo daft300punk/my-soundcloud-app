2b4d192c0e2ccc8576a672184c34ed4e136eed3d
Abhilash Singh <daft300punk@gmail.com>
2017-06-01 19:31:14 +0530

    Update LeftSidebarContainer component

--- src/containers/LeftSidebarContainer/index.js
+++ src/containers/LeftSidebarContainer/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import LeftSidebar from '../../components/LeftSidebar';
+import PropTypes from 'prop-types';
+import changeCategory from '../../actions/categoryAction';
 
-const LeftSidebarContainer = () => (
-  <LeftSidebar />
-);
+const LeftSidebarContainer = ({
+  changeCategory,
+  kind,
+  trackType
+}) => {
+  return (
+    <LeftSidebar 
+      changeCategory={changeCategory}
+      kind={kind}
+      trackType={trackType}
+    />
+  );
+};
+
+LeftSidebarContainer.propTypes = {
+  changeCategory: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  kind: state.category.kind,
+  trackType: state.category.trackType
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeCategory: (kind, genre, trackType) => dispatch(changeCategory(kind, genre, trackType))
+});
 
 export default connect(
+  mapStateToProps,
+  mapDispatchToProps
 )(LeftSidebarContainer);
